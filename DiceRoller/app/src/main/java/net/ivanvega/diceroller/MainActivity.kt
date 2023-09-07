0a00864317f1f9cc63d54d5a9d2cee9bb10345aa
arturo <s20120174@alumnos.itsur.mx>
2023-09-06 21:29:58 -0600

    Ya jala con 2 dados

--- DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -42,35 +43,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
-    var result by remember {
+    var result1 by remember {
         mutableStateOf(1)
     }
-
-    val imageResource = when(result){
-        1 -> R.drawable.dice_1
-         2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
+    var result2 by remember {
+        mutableStateOf(1)
     }
 
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(painter = painterResource(id = imageResource) ,
+    )
+    {
+        Image(painter = painterResource(id = roll(result = result1)) ,
             contentDescription = stringResource(
-            R.string.imagen_de_dado
+                R.string.imagen_de_dado
             )
         )
+        Image(painter = painterResource(id = roll(result = result2)) ,
+            contentDescription = stringResource(
+                R.string.imagen_de_dado
+            )
+        )
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
+        Button(onClick = { result1 = (1..6).random()
+            result2 = (1..6).random()}) {
             Text(stringResource(R.string.roll))
         }
     }
 
 
+}
+
+//@Composable
+private fun roll(result:Int):Int{
+
+    return  when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
 }
 
 @Preview
